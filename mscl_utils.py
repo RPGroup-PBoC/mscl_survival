daf9e25c32f5f145a996aa2f894a26546faf3383
Griffin Chure <gchure@caltech.edu>
2017-05-29 20:55:19 -0700

    Worked on a threshold based segmentation and refined lineage mapping

--- mscl_utils.py
+++ mscl_utils.py
@@ -1,11 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import skimage.morphology
+import glob
+import mscl_utils as mscl
+
+import skimage.io
 import skimage.measure
+import skimage.morphology
+import skimage.segmentation
+import skimage.filters
+import scipy.ndimage
+
+import pandas as pd
+import xmltodict
+import json
 
 
-def contour_seg(image, level, selem='default'):
+def contour_seg(image, level=0.3, selem='default', perim_bounds=(5, 25),
+                ip_dist=0.160, return_conts=False):
     """
     Identifies contours around dark objects in a phase contrast image.
 
@@ -15,29 +27,504 @@ def contour_seg(image, level, selem='default'):
         Phase contrast image of interest. This shoul
     level: float
         Level at which to draw contours on black top-hat filtered image.
-        Default value is 4000. This is dependent on the image, but should
-        be robust for images of similar illumination.
+        Default value is 0.3.
     selem: 2d-array or string
         Structuring element to use for the black top-hat filtering procedure
         Default value is a disk with a diameter of 20 pixels.
+    perim_bounds: length 2 tuple
+        Lower and upper perimeter bounds of approved objects. This should be
+        in units of microns. The default values are 5 and 25 microns for the
+        lower and upper bound, respectively.
+    ip_dist : float
+        Interpixel distance of the image in units of microns per pixel. The
+        default value is 0.160 microns per pixel.
+    return_conts : bool
+        If True, the x and y coordinates of the individual contours will be
+        returned. Default value is False
 
     Returns
     -------
+    im_lab : 2d-array, int
+        Two dimensional image where each individual object is labeled.
+
     conts : 1d-array
         List of contour coordinates. Each entry of this array comes as
         an x,y pair of arrays. Has the same length as the number of
-        contoured objects
+        contoured objects. This is only returned if `return_conts` is
+        True.
 
     """
 
     # Apply the white top-hat filter.
     if selem == 'default':
         selem = skimage.morphology.disk(20)
-    im_filt = skimage.morphology.black_tophat(image, selem)
+
+    # Normalize the image.
+    image = (image - image.min()) / (image.max() - image.min())
+
+    # Blur and background subtract the image.
+    im_blur = skimage.filters.gaussian(image, sigma=5)
+    im_sub = image - im_blur
+
+    # Apply the black tophat filter.
+    im_filt = skimage.morphology.black_tophat(im_sub, selem)
 
     # Find the contours and return.
     conts = skimage.measure.find_contours(im_filt, level)
-    return conts
+
+    # Make an empty image for adding the approved objects.
+    objs = np.zeros_like(image)
+
+    # Loop through each contour.
+    for _, c in enumerate(conts):
+        perim = 0
+        for j in range(len(c) - 1):
+            # Compute the distance between points.
+            distance = np.sqrt((c[j+1, 0] - c[j, 0])**2 +
+                               (c[j+1, 1] - c[j, 1])**2)
+            perim += distance * ip_dist
+
+        # Test if the perimeter is allowed by the user defined bounds.
+        if (perim > perim_bounds[0]) & (perim < perim_bounds[1]):
+
+            # Round the contours.
+            c_int = np.round(c).astype(int)
+
+            # Color the image with the contours and fill.
+            objs[c_int[:, 0], c_int[:, 1]] = 1.0
+
+    # Fill and label the objects.
+    objs_fill = scipy.ndimage.binary_fill_holes(objs)
+    objs_fill = skimage.morphology.remove_small_objects(objs_fill)
+    objs_border = skimage.segmentation.clear_border(objs_fill)
+    im_lab = skimage.measure.label(objs_border)
+    if return_conts is True:
+        return conts, im_lab
+    else:
+        return im_lab
+
+
+def threshold_seg(image, area_bounds=(1, 20), ip_dist=0.16,
+                  blur_radius=30):
+    """
+    Segments dark objects in a phase contrast image using the
+    Otsu thresholding algorithm.
+
+    Parameters
+    ----------
+    image : 2d-array
+        Phase contrast image to be segmented.
+    area_bounds : tuple
+        Lower and upper area bounds for filtering objects by area.
+        This should be in units of square microns. Default values
+        are 0.5 and 20 square microns for the lower and upper
+        bounds respectively.
+    ip_dist : float
+        Interpixel distance of the image. This should be in units
+        of microns per pixel. Default value is 0.16 microns per pixel.
+    blur_radius : int
+        Radius for the gaussian blur performed during background
+        subtraction. This is in units of pixels. Default value is
+        30 pixels.
+
+    Returns
+    -------
+    im_lab : 2d-array of int
+        The final segmentation mask with individually labeled objects.
+    """
+    # Make sure the image is normalized.
+    if image.max() > 1:
+        image = (image - image.min()) / (image.max() - image.min())
+
+    # Perform the background subtraction.
+    im_blur = skimage.filters.gaussian(image, blur_radius)
+    im_sub = image - im_blur
+
+    # Threshold using Otsu's method.
+    # thresh = skimage.filters.threshold_otsu(im_sub)
+    thresh = -0.1
+    im_thresh = im_sub < thresh
+
+    # Fill the holes and filter by area.
+    im_fill = scipy.ndimage.binary_fill_holes(im_thresh)
+    im_lab = skimage.measure.label(im_fill)
+    props = skimage.measure.regionprops(im_lab)
+    obj = np.zeros_like(im_thresh)
+    for prop in props:
+        area = prop.area * ip_dist**2
+        if (area > area_bounds[0]) & (area < area_bounds[1]):
+            obj += (im_lab == prop.label)
+
+    # Relabel and return.
+    im_lab = skimage.measure.label(obj)
+    return im_lab
+
+def marker_parse(fname, type_dict={1: False, 2: True}):
+    """
+    Parses the XML file produced from the CellCounter ImageJ plugin and
+    packages the marker positions and type into a Pandas DataFrame.
+
+    Parameters
+    ----------
+    fname : str
+        Path to the XML file of interest.
+    type_dict : dict
+        Dictionary of types and survival. Default is assigning type 1
+        as death and type 2 as survival.
+
+    Returns
+    -------
+    df : Pandas DataFrame
+        Data frame containing x and y positions of markers as well as
+        the type classification.
+    """
+    with open(fname, 'r') as f:
+        positions = xmltodict.parse(f.read())
+
+    # Extract only the marker data.
+    markers =  positions['CellCounter_Marker_File']['Marker_Data']['Marker_Type']
+
+    # Find the total number of types and loop through them to make data frames.
+    dfs = []
+    num_types = len(markers)
+    for i in range(num_types):
+        try:
+            type_marks = markers[i]['Marker']
+            _df = pd.DataFrame(type_marks)
+            # Insert a column keeping track of the type
+            _df.insert(0, 'survival', type_dict[int(markers[i]['Type'])])
+            dfs.append(_df)
+        except:
+            pass
+
+    # Concatenate the data frames ignorning indexing.
+    df = pd.concat(dfs, axis=0, ignore_index=True)
+
+    # Clean up the data frame and return.
+    df.drop('MarkerZ', 1, inplace=True)
+    df.columns = ['survival', 'x_pos', 'y_pos']
+    df['x_pos'] = df['x_pos'].astype(int)
+    df['y_pos'] = df['y_pos'].astype(int)
+    return df
+
+
+def link_markers(markers, seg_mask, fluo_image, ip_dist=0.160,
+                 return_coords=False, inplace=False,
+                 position_labels=('x_pos', 'y_pos')):
+    """
+    Maps markers from one image to centroids of segmented objects from
+    another. This assumes a marker belongs to the object with the minimum
+    marker-centroid distance.
+
+    Parameters
+    ----------
+    markers : Pandas DataFrame
+        DataFrame containing the x and y positions of the markers.
+    seg_mask : 2d-array, int
+        Labeled segmentation mask. The coordinates of the object centroids
+        will be calculated from this image.
+    fluo_image : 2d-array, float or int
+        The fluorescence image used to extract intensities. If None, no
+        intensity information will be returned. These intensity values
+        will be returned as an intensity per square physical distance
+        as given by `ip_dist`.
+    ip_dist :  float
+        Interpixel distance for the image. Default value is 0.160 microns
+        per pixel.
+    return_coords : bool
+        If True, the paired coordinates will be returned as a tuple. It
+        will have the form ((mark_x, mark_y), (cent_x, cent_y)). Default
+        value is False.
+    inplace : bool
+        If True, the markers DataFrame will be updated in place with the
+        paired mask label and intensity if the fluorescence image is given.
+    position_labels :  tuple of str
+        Labels of position markers in the markers DataFrame in the order
+        of x position and y position. Default is `x_pos` and `y_pos`.
+
+   Returns
+   -------
+   df : Pandas DataFrame
+       DataFrame containing survival type, marker positions, mask label,
+       area, and intensity if provided. Note this is not returned if
+       `in_place = True`.
+   coords : list of tuple
+       A list of tuples containing the marker x,y positions and the
+       coordinates of the associated segmentation centroid. This
+       is only returned if `return_coords`==True.
+
+    """
+    # Compute the properties from the segmentation mask.
+    props = skimage.measure.regionprops(seg_mask, fluo_image)
+    area, intensity, labels, centroids = [], [], [], []
+    for prop in props:
+        area.append(prop.area * ip_dist**2)
+        intensity.append(prop.mean_intensity / ip_dist**2)
+        labels.append(prop.label)
+        centroids.append(prop.centroid)
+
+    # Set up a list to store the coordinates and duplicate the df.
+    coords = []
+    df = markers.copy(deep=True)
+
+    # Compute the minimum distances.
+    for i in range(len(markers)):
+        distances = []
+        x = markers.iloc[i][position_labels[0]]
+        y = markers.iloc[i][position_labels[1]]
+
+        # Loop through each centroid and find the minimum distance.
+        for c in centroids:
+            distances.append(np.sqrt((x - c[1])**2 + (y - c[0])**2))
+
+        # Find the index with the minimum distance.
+        min_ind = np.argmin(distances)
+        coords.append(((x, y),
+                       (centroids[min_ind][1], centroids[min_ind][0])))
+
+        # Determine if a new DataFrame should be made or not.
+        # There should be a better way to do this -- will spruce up later.
+        if inplace is False:
+            # Update the data frame.
+            df.set_value(i, 'mask_label', labels[min_ind])
+            df.set_value(i, 'label_cent_x', centroids[min_ind][1])
+            df.set_value(i, 'label_cent_y', centroids[min_ind][0])
+            df.set_value(i, 'intensity', intensity[min_ind])
+            df.set_value(i, 'area', area[min_ind])
+
+        else:
+            markers.set_value(i, 'mask_label', labels[min_ind])
+            markers.set_value(i, 'label_cent_x', centroids[min_ind][1])
+            markers.set_value(i, 'label_cent_y', centroids[min_ind][0])
+            markers.set_value(i, 'intensity', intensity[min_ind])
+            markers.set_value(i, 'area', area[min_ind])
+
+    # Figure out what to return.
+    if inplace is False:
+        if return_coords is True:
+            return df, coords
+        else:
+            return df
+
+
+def scrape_metadata(fname, channels=('Brightfield', 'GFP'), return_date=True):
+    """
+    Takes an image metadata file and returns the datea nd GFP exposure time.
+
+    Parameters
+    ----------
+    fname : str
+        Pat of the metadata file to parse.
+    channel : tuple of str
+        The channels from which to scrape the exposure time. A single channel
+        name can be given. Default is ('Brightfield', 'GFP').
+    return_date : bool
+        If True, the date of the acquisition will also be returned.
+
+    Returns
+    -------
+    exposure: dict or float
+        The exposure time of the desired channel. If multiple channels are
+        given, this will be a tuple of the exposure times. If return_date is
+        True, the date will also be in this dictionary.
+   """
+
+    # Open the metadata file.
+    with open(fname, 'r') as f:
+        metadata = json.load(f)
+
+    # Get the date from the Summary field.
+    date = metadata['Summary']['Date'].split('-')
+    date = ''.join(date)
+
+    # Get a list of the keys in the metadata file.
+    keys = metadata.keys()
+
+    # Determine if a single channel or multiple channel exposures are desired.
+    if (type(channels) != tuple) & (type(channels) != str):
+        raise TypeError('desired channels must be a tuple or a string.')
+    else:
+        if type(channels) == str:
+            num_channels = 1
+            channels = (channels)
+            exposure = None
+        else:
+            num_channels = len(channels)
+            exposure = []
+
+    # Loop through each desired channel and scrape the exposure.
+    for i in range(num_channels):
+        for k in keys:
+            try:
+                chan = metadata[k]['Channel']
+                if chan.lower() == channels[i].lower():
+                    _exposure = metadata[k]['Exposure-ms']
+                if num_channels == 1:
+                    exposure = _exposure
+                else:
+                    if i == 0:
+                        exposure = {channels[i] + '_exp_ms': _exposure}
+                    else:
+                        exposure[channels[i] + '_exp_ms'] = _exposure
+            except:
+                pass
+
+    if return_date is True:
+        exposure['date'] = date
+    return exposure
+
+
+def save_seg(fname, image, mask, fill_contours=True, ip_dist=0.160,
+             bar_length=10, title=None, colormap='hls'):
+    """
+    Saves a merge of a segmentation mask and the original image for a
+    sanity check.
+
+    Parameters
+    ----------
+    fname : str
+        The file will be saved with this path.
+    image : 2d-array, float
+        The original image on which the segmentation mask will be overlaid.
+    mask : 2d-array, bool
+        Boolean segmentation mask of the original image.
+    contours: bool
+        If True, contours of segmented objects will be filled.
+    ip_dist : float
+        Interpixel distance for the image. This is used for computing the
+        scalebar length.  This should be in units of microns. Default
+        value is 0.160 microns per pixel.
+    bar_length : int
+        The length of the desired scalebar in units of microns.
+    title : str, optional
+        Title for the image.
+    colormap : str
+        Colormap for labeling the objects. Default is the high-contrast
+        'hls'. This can take any standard colormap string.
+
+    Return
+    ------
+    fig : Matplotlib Figure object
+        Figure containing the axis of the plotted image.
+    """
+
+    # Make copies of the image and mask.
+    image_copy = np.copy(image)
+    mask_copy = np.copy(mask)
+
+    # Burn the scalebar into the upper-left hand  of th image.
+    num_pix = int(bar_length / ip_dist)
+    image = (image_copy - image_copy.min()) /\
+            (image_copy.max() - image_copy.min())
+    image[10:20, 10:10 + num_pix] = 1.0
+
+    # Make sure the mask is a boolean image.
+    if type(mask) != bool:
+        mask = mask_copy > 0
+
+    # Find the contours of the mask.
+    conts = skimage.measure.find_contours(mask, 0)
+
+    # Plot the image and generate the contours.
+    with sns.axes_style('white'):
+        fig = plt.figure()
+        plt.imshow(image, cmap=plt.cm.Greys_r)
+
+        # Plot all of the contours
+        colors = sns.color_palette(colormap, n_colors=len(conts))
+        for i, c in enumerate(conts):
+            plt.plot(c[:, 1], c[:, 0], color=colors[i], lw=0.75)
+            if fill_contours is True:
+                plt.fill(c[:, 1], c[:, 0], color=colors[i], alpha=0.5)
+
+        # Remove the axes.
+        plt.xticks([])
+        plt.yticks([])
+
+        # Add title if provided.
+        if title is not None:
+            plt.title(title)
+
+        # Tighten up and save the image.
+        plt.tight_layout()
+        plt.savefig(fname, bbox_inches='tight')
+        plt.close()
+    return fig
+
+
+def show_connections(fname, image, data, title=None, bar_length=10,
+                     ip_dist=0.16):
+    """
+    Saves the original phase contrast image with the segmented
+    centroids and the manually recorded markers linked by lines.
+
+    Parameters
+    ----------
+    fname : str
+        Filename to save the image wish shown connections between
+        segmented object centroids and the markers.
+    image : 2d-array
+        Original phase contrast image over which the points will
+        be drawn
+    data : Pandas DataFrame
+        DataFrame containing the marker x and y positions and the
+        centroid x and y positions.
+    title : str
+        Title to be applied to the image. If not specified, none will
+        be included.
+    bar_length : int
+        Length of the scalebar in units of microns. Default value
+        is 10.
+    ip_dist : float
+        Interpixel distance of the image. This should be in units of
+        microns per pixel. Default value is 0.16 microns per pixel.
+
+    Returns
+    -------
+    fig : Matplotlib Figure Canvas
+        Figure canvas of the plot.
+    """
+    # Add the scale bar to the image.
+    if image.max() > 1:
+        image = (image - image.min()) / (image.max() - image.min())
+    num_pix = int(bar_length / ip_dist)
+    image_copy = np.copy(image)
+    image_copy[10:20, 10:10 + num_pix] = 1.0
+
+    # Define the colors for survivors and corpses.
+    colors = {False: '#D56C55', True: '#08AADE'}
+
+    # Group the DataFrame by survival.
+    grouped = pd.groupby(data, 'survival')
+
+    # Show the image
+    with sns.axes_style('white'):
+        fig = plt.figure()
+        plt.imshow(image_copy, cmap=plt.cm.Greys_r)
+        plt.plot([], [], '-o', ms=3, lw=1, color=colors[True],
+                 label='survivor')
+        plt.plot([], [], '-o', ms=3, lw=1, color=colors[False], label='goner')
+        plt.legend(loc='lower left')
+        for g, d in grouped:
+            for i in range(len(d)):
+                # Parse the positions
+                m_x = d.iloc[i]['x_pos']
+                m_y = d.iloc[i]['y_pos']
+                c_x = d.iloc[i]['label_cent_x']
+                c_y = d.iloc[i]['label_cent_y']
+                # Plot the connections.
+                plt.plot((m_x, c_x), (m_y, c_y), 'o-', ms=3, lw=1,
+                         color=colors[g])
+
+        # Format the axes
+        plt.xticks([])
+        plt.yticks([])
+
+        # Add a title if necessary.
+        if title is not None:
+            plt.title(title, fontsize=12)
+        plt.savefig(fname, bbox_inches='tight')
+    return fig
 
 
 def set_plotting_style(return_colors=True):
