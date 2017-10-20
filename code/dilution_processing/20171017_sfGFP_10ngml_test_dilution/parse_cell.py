c3243a2255933d24555fa3019548a13e7ccde02c
Griffin Chure <gchure@caltech.edu>
2017-10-20 14:10:48 -0700

    Working on linking the lineages from a df. Commit before I start over

--- code/dilution_processing/20171017_sfGFP_10ngml_test_dilution/parse_cell.py
+++ code/dilution_processing/20171017_sfGFP_10ngml_test_dilution/parse_cell.py
@@ -104,10 +104,69 @@ for g, d in grouped:
 fig, ax = plt.subplots(1, 1)
 ax.loglog(dilution_df['summed_int'], dilution_df['square_difference'],
           'o', color='slategray', alpha=0.5)
-j
+
 
 #%%
 # Try to link up conservation of fluorescence from birth to death.
 # Take only one position.
-xy01 = growth_df[growth_df['position'] == '01']
-xy01.loc[xy01['birth_frame'] == 1]
+xy01 = growth_df.loc[growth_df['position'] == '01']
+
+# Look only at cells that divided or died on last frame.
+producers = xy01.loc[(xy01['division'] == True) & (xy01['death_frame'] != 26)]
+
+#  | ((xy01['death_frame']==26) & (xy01['birth_frame'] != 1))]
+last_generation = xy01[(xy01['death_frame'] == 26) &
+                       (xy01['birth_frame'] != 1)]
+
+# Insert an empty tree number.
+producers.loc[:, 'tree_number'] = np.nan
+last_generation.loc[:, 'tree_number'] = np.nan
+
+
+# Set the tree roots.
+producers.loc[producers['mother_ID'] == 0, 'tree_number'] = producers['ID']
+
+# %% it_counter = 0
+it_counter = 0
+orphans = producers.isnull().values.any()
+while (orphans == True) & (it_counter < 20):
+    # Grab all of the NaN and non NaN values.
+    unassigned = producers[producers.isnull().any(axis=1)]
+
+    num_before = len(unassigned)
+    assigned = producers[~producers.isnull().any(axis=1)]
+    grouped = assigned.groupby('tree_number')
+    for g, d in grouped:
+        # Get a list of the daughter IDs.
+        daughters = d[['daughter_1_ID', 'daughter_2_ID']].values.flatten()
+
+        # Change the tree number in the producers. for those indices.
+        for progeny in daughters:
+            producers.loc[producers['ID'] == progeny, 'tree_number'] = g
+    orphans = producers.isnull().values.any()
+    orphans
+    print(it_counter)
+    it_counter += 1
+
+
+# Link the final frame.
+grouped == producers.groupby('tree_number')
+for g, d in grouped:
+    daughters = d[['daughter_1_ID', 'daughter_2_ID']].values.flatten()
+    for progeny in daughters:
+        last_generation.loc[last_generation['ID']
+                            == progeny, 'tree_number'] = g
+
+lineages = pd.concat([producers, last_generation], axis=0)
+
+print('finished!')
+# %% I'm skeptical this is working with just a single iteration.
+# Ignore the ends for now.
+complete_lineages = lineages.dropna()
+complete_lineages.head()
+# Group by the lineage information.
+family_tree = complete_lineages.groupby('tree_number')
+for g, d in family_tree:
+    d
+
+#
