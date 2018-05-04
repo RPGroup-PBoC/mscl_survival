dd89b42de25ebafbfb226dea1151470f63981aef
Griffin Chure <gchure@caltech.edu>
2018-05-04 14:19:08 -0700

    Added work from delbruck

--- code/analysis/complete_mcmc_pooled.py
+++ code/analysis/complete_mcmc_pooled.py
@@ -7,6 +7,7 @@ import pystan
 sys.path.insert(0, '../../')
 import mscl.stats
 import mscl.mcmc
+import glob
 
 # Define the parameters from Bialecka-Fornal et al.  2012
 CHANNEL_NUM = 340
@@ -41,7 +42,7 @@ data['scaled_intensity'] = (
 data = data[data['scaled_intensity'] >= 0].copy()
 
 # Load the stan model.
-#model = pystan.StanModel('../stan/complete_analysis.stan')
+model = pystan.StanModel('../stan/complete_analysis.stan')
 
 
 # Assemble the data dictionary.
@@ -55,14 +56,14 @@ data_dict = dict(J1=6, J2=1, N1=len(data[data['rbs'] == 'mlg910']), N2=len(data[
                  survival=data[data['rbs'] != 'mlg910']['survival'].values.astype(int))
 #%% Sample
 print('sampling...')
-samples = model.sampling(data=data_dict, iter=5000, chains=4, n_jobs=-1)
+samples = model.sampling(data=data_dict, iter=8000, chains=4, n_jobs=-1)
 print('finished!')
 
 # %% Convert to a DataFrame and and save.
 sample_df = mscl.mcmc.chains_to_dataframe(samples)
 stats = mscl.mcmc.compute_statistics(sample_df)
-sample_df.to_csv('../../data/csv/complete_mcmc_traces.csv', index=False)
-stats.to_csv('../../data/csv/complete_mcmc_stats.csv', index=False)
+sample_df.to_csv('../../data/csv/pooled_complete_mcmc_traces.csv', index=False)
+stats.to_csv('../../data/csv/pooled_complete_mcmc_stats.csv', index=False)
 
 # Piece together the final data frame
 shock_data = data[data['rbs'] != 'mlg910'].copy()
@@ -70,10 +71,10 @@ cal_data = data[data['rbs']=='mlg910'].copy()
 shock_data.drop(axis=1, labels=['exposure_ms', 'mean_bg', 'idx', 'intensity', 'replicate_number'], inplace=True)
 
 for j in range(2):
-    beta_stats = stats[stats['parameter'] == 'beta_{}__{}'.format(j, rate_keys[r])].values[0][1:]
-    shock_data.loc[:, 'beta_{}_median'.format(j)] = beta_stats[0] 
-    shock_data.loc[:, 'beta_{}_min'.format(j)] = beta_stats[1] 
-    shock_data.loc[:, 'beta_{}_max'.format(j)] = beta_stats[2] 
+	beta_stats = stats[stats['parameter'] == 'beta_{}'.format(j)].values[0][1:]
+	shock_data.loc[:, 'beta_{}_median'.format(j)] = beta_stats[0] 
+	shock_data.loc[:, 'beta_{}_min'.format(j)] = beta_stats[1] 
+	shock_data.loc[:, 'beta_{}_max'.format(j)] = beta_stats[2] 
 
 # Include the channel copy number info.
 median_n = [stats[stats['parameter']=='n_sr__{}'.format(i)]['median'].values[0] for i in range(len(shock_data))]
@@ -88,4 +89,4 @@ shock_data.loc[:, 'minimum_calibration_factor'] = stats[stats['parameter']=='hyp
 shock_data.loc[:, 'maximum_calibration_factor'] = stats[stats['parameter']=='hyper_alpha_mu']['hpd_max'].values[0]
 
 # Save the final dataframe.
-shock_data.to_csv('../../data/csv/mscl_survival_data.csv', index=False)
+shock_data.to_csv('../../data/csv/pooled_mscl_survival_data.csv', index=False)
